6f28c652237eb8d9378ec7b4ff2d1597af6e92b1
AMIN TROUDI <troudima69@gmail.com>
2025-12-24 16:51:11 +0100

    Ajout Sidemenu

---
+++ 1project/1project/src/Sidemenu.js
@@ -0,0 +1,10 @@
+export default function Sidemenu(){
+    const style={
+        
+    }
+    return(
+        <div style={{backgroundColor:"red" ,marginBottom:"500px" ,display:"flex", marginTop:"auto",marginBottom:"auto"  }}>
+            <button>click me</button>
+        </div>
+    );
+}
