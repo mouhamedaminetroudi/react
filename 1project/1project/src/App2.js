497edb358853e240a4b1cb10062ad1677753906d
AMIN TROUDI <troudima69@gmail.com>
2025-12-27 18:02:34 +0100

    app2

--- 1project/1project/src/App2.js
+++ 1project/1project/src/App2.js
@@ -1,22 +1,45 @@
-import Article from "./Article";
+import Heade2 from "./Heade2";
+import Post2 from "./Post2";
+import './App2.css';
+import Sidemenu2 from "./Sidemenu2";
 function App2(){
-    const firstArticle=`
-    sqdddddddddddddddddddddddddq
-    dsqdsdqdqsdsd`
-
-    const secondArticle=`qsqsaaazzzzzzzzzzzzzzzzzzza
-    azeaaezeaea`
+const com1="acadimiti tourahab bikom"
+const com2="this is the hello world articl"
+const com3="this is the body of post 3"
 return(
-    <div>
-        <Article para={firstArticle} name="AHMED" email="Ahmed@gmail.com"><h1>hello world</h1></Article>
-        <Article para={secondArticle} name="AMIN" email="AMIN@gmail.com"><div style={{backgroundColor:"teal"}}>
-            <h2>helooooo</h2>
-            <p style={{backgroundColor:"orange"}}>sala yaa salih</p>
-            </div></Article>
-        <Article name="SALH" email="SALH@gmail.com"/>
-        <Article name="CHOUCHA3" email="CHOUCHA3@gmail.com"/>
+    
+    <div className="layout">
+        <div >
+        <Heade2/>
+        </div>
+        <div className="main">
+            <Post2 com={com1}>
+                <div>
+                <h2>20</h2>
+                <h3>ACADIMY AMIN</h3>
+                
+                
+                </div>
+            </Post2>
+
+            <Post2 com={com2}>
+                <div>
+                    <h2>Heloo world</h2>
+                </div>
+            </Post2>
+            <Post2 com={com3} style={{float:"left"}}>
+                <h2>Post 3</h2>
+
+            </Post2>
+
+        </div>
+        <div>
+            <Sidemenu2></Sidemenu2>
+        </div>
+
 
     </div>
+    
 );
 
 }
