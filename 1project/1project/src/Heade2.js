4c9d6762f404d0207c05db1defaf47fe4cbcf309
AMIN TROUDI <troudima69@gmail.com>
2025-12-27 18:03:09 +0100

    header

---
+++ 1project/1project/src/Heade2.js
@@ -0,0 +1,18 @@
+export default function(){
+    const style={
+        backgroundColor:"teal",
+        textAlign:"center",
+        color:"white",
+        display:"flex",
+        justifyContent:"center",
+        elaignItems:"center",
+        height:"130px",
+        boxShadow:"0px 0px 10px black"
+    }
+    return (
+        <div style={style}>
+            
+            <h1>AMIN's ACADIMY</h1>
+        </div>
+    );
+}
