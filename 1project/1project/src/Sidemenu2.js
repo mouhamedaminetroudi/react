bb439f77edc0b17de000c532aedc26dec04a2345
AMIN TROUDI <troudima69@gmail.com>
2025-12-27 18:03:33 +0100

    header

---
+++ 1project/1project/src/Sidemenu2.js
@@ -0,0 +1,7 @@
+export default function(){
+    return(
+        <div style={{width:"20%",border:"5px solid teal",float:"right",marginBottom:"40px" ,display:"flex",} }className="sidemenu">
+            <button className="bt1">click</button>
+        </div>
+    );
+}
